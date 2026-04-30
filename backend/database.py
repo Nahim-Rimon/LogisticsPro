12b2768cc7079307eb331d2f4f0de3ce4a8cbed1
Nahim Ahmed Rimon <nahim.itsd@goldenharvestbd.com>
2026-04-30 13:37:14 +0600

    add getenv to reduct env pull

--- backend/database.py
+++ backend/database.py
@@ -4,8 +4,8 @@ from supabase import create_client, Client
 
 load_dotenv()
 
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
 
 if not url or not key:
     raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set")
