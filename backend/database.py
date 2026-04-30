d311b0666e96bf4a74928b1384fa96b3a73cd7fd
Nahim Ahmed Rimon <nahim.itsd@goldenharvestbd.com>
2026-04-30 13:31:25 +0600

    update database.py file

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,14 @@
 import os
-from supabase import create_client, Client
 from dotenv import load_dotenv
+from supabase import create_client, Client
 
 load_dotenv()
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+
+if not url or not key:
+    raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set")
 
 supabase: Client = create_client(url, key)
 
